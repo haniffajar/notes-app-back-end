2654ee93187381e5f22e0b5d8636144601668408
haniffajar <hanif.fajarnur@gmail.com>
2023-09-22 10:06:16 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+// npm init --y
+// npm install nodemon --save-dev
+// npm install eslint --save-dev
+// npx eslint --init
+// npm install @hapi/hapi
+// npm install nanoid@3.x.x
+
+const Hapi = require('@hapi/hapi');
+
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
